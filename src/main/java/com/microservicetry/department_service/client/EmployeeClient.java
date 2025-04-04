d35f9016e87876ffb9bea43ef6fdddc1bdbf603b
Kevin Llanos <kllano2@wgu.edu>
2025-04-04 16:33:29 -0500

    tested data through postman after finishing WebClientConfig

--- src/main/java/com/microservicetry/department_service/client/EmployeeClient.java
+++ src/main/java/com/microservicetry/department_service/client/EmployeeClient.java
@@ -2,7 +2,6 @@ package com.microservicetry.department_service.client;
 
 import com.microservicetry.department_service.model.Employee;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
